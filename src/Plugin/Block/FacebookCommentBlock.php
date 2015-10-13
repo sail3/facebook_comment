435aa41896484c82b0ffe6e703bf3f295f52940d
Pedro charun <mlosail3@gmail.com>
2015-10-13 00:20:24 -0500

    Dev: configuration form module

--- src/Plugin/Block/FacebookCommentBlock.php
+++ src/Plugin/Block/FacebookCommentBlock.php
@@ -8,6 +8,7 @@ namespace Drupal\facebook_comment\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Block\BlockPluginInterface;
 use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Provides a Facebook Comment Block.
  * @Block(
@@ -16,6 +17,7 @@ use Drupal\Core\Form\FormStateInterface;
  * )
  */
 class FacebookCommentBlock extends BlockBase implements BlockPluginInterface {
+
   /**
   * {@inheritdoc}
   */
@@ -23,23 +25,14 @@ class FacebookCommentBlock extends BlockBase implements BlockPluginInterface {
     $build = [];
     $request = \Drupal::request();
     $config = $this->getConfiguration();
-    dpm($request->getUri());
-    // $build['facebook_comment']['#markup'] = "facebook.com";
     $build[] = [
       "#theme" => "facebook_comment",
-      "#description" => "foo",
-      "#appId" => $config['fc_app_id'],
-      "#appUrl" => "hola mundo",
-
-      "#applicationCode" => $config['fc_applicationCode'],
-      "#pageUrl" => $config['fc_pageUrl'],
       "#pageUrl" => $request->getUri(),
-      "#numPosts" => $config['fc_numPosts'],
-      "#width" => $config['fc_width'],
-      "#orderBy" => $config['fc_orderBy'],
-      "#colorScheme" => $config['fc_colorScheme'],
-
-      "#attributes" => [],
+      "#applicationCode" => isset($config['fc_applicationCode']) ? $config['fc_applicationCode'] : "",
+      "#numPosts" => isset($config['fc_numPosts']) ? $config['fc_numPosts'] : "10",
+      "#width" => isset($config['fc_width']) ? $config['fc_width'] : "100%",
+      "#orderBy" => isset($config['fc_orderBy']) ? $config['fc_orderBy'] : "social",
+      "#colorScheme" => isset($config['fc_colorScheme']) ? $config['fc_colorScheme'] : "light",
       "#attached" => [
         'library' => [
           "facebook_comment/fb-comment",
@@ -48,59 +41,61 @@ class FacebookCommentBlock extends BlockBase implements BlockPluginInterface {
     ];
     return $build;
   }
+
+  /**
+  * {@inheritdoc}
+  */
   public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
     $config = $this->getConfiguration();
-    $form['fc_app_id'] = array(
-     '#type' => 'textfield',
-     '#title' => $this->t("App Id"),
-     '#description' => $this->t("Insert a app id from facebook."),
-     '#default_value' => isset($config['fc_app_id']) ? $config['fc_app_id'] : "",
-    );
     $form['fc_applicationCode'] = array(
       '#type' => 'textfield',
       '#title' => $this->t("Aplication Code"),
-      '#description' => $this->t("Dumi description"),
+      '#description' => $this->t("Facebook App-id. If you do not know this see module information."),
       '#default_value' => isset($config['fc_applicationCode']) ? $config['fc_applicationCode'] : "",
-    );
-    $form['fc_pageUrl'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t("dummi text"),
-      '#description' => $this->t("Dumi description"),
-      '#default_value' => isset($config['fc_pageUrl']) ? $config['fc_pageUrl'] : "",
+      '#required' => TRUE,
     );
     $form['fc_numPosts'] = array(
       '#type' => 'textfield',
-      '#title' => $this->t("Number post display"),
-      '#description' => $this->t("Dumi description"),
+      '#title' => $this->t("Number comment display"),
+      '#description' => $this->t("Number of comment display per page"),
       '#default_value' => isset($config['fc_numPosts']) ? $config['fc_numPosts'] : "10",
     );
     $form['fc_width'] = array(
       '#type' => 'textfield',
       '#title' => $this->t("Width size"),
-      '#description' => $this->t("Dumi description"),
+      '#description' => $this->t("Maximum size for display on the page, if the size is in percent must include the symbol ( % )."),
       '#default_value' => isset($config['fc_width']) ? $config['fc_width'] : "100%",
     );
     $form['fc_orderBy'] = array(
-      '#type' => 'textfield',
+      '#type' => 'radios',
       '#title' => $this->t("Order By"),
-      '#description' => $this->t("Dumi description"),
+      '#options' => [
+        'social' => $this->t('Social'),
+        'reverse_time' => $this->t('Desc'),
+        'time' => $this->t('Asc'),
+      ],
+      '#description' => $this->t("Select an order to display Comments."),
       '#default_value' => isset($config['fc_orderBy']) ? $config['fc_orderBy'] : "social",
     );
     $form['fc_colorScheme'] = array(
-      '#type' => 'textfield',
+      '#type' => 'radios',
       '#title' => $this->t("Color Schema"),
-      '#description' => $this->t("Dumi description"),
+      '#options' => [
+        'light' => $this->t('Light'),
+        'dark' => $this->t('Dark'),
+      ],
+      '#description' => $this->t("Select a theme for display comment block."),
       '#default_value' => isset($config['fc_colorScheme']) ? $config['fc_colorScheme'] : "light",
     );
     return $form;
   }
-  public function blockSubmit($form, FormStateInterface $form_state)
-  {
-    $this->setConfigurationValue('fc_app_id', $form_state->getValue('fc_app_id'));
 
+  /**
+  * {@inheritdoc}
+  */
+  public function blockSubmit($form, FormStateInterface $form_state) {
     $this->setConfigurationValue('fc_applicationCode', $form_state->getValue('fc_applicationCode'));
-    $this->setConfigurationValue('fc_pageUrl', $form_state->getValue('fc_pageUrl'));
     $this->setConfigurationValue('fc_numPosts', $form_state->getValue('fc_numPosts'));
     $this->setConfigurationValue('fc_width', $form_state->getValue('fc_width'));
     $this->setConfigurationValue('fc_orderBy', $form_state->getValue('fc_orderBy'));
